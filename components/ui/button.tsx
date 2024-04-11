905066c7bb008785af409ba2bd90acb183b08d7b
MagicSakuraD <ckgoforit@outlook.com>
2024-04-11 17:16:49 +0800

    Update button styles and add new variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,10 +15,21 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost:
+          "hover:bg-slate-100 bg-transparent text-slate-500 border-transparent border-0",
         link: "text-primary underline-offset-4 hover:underline",
+        primary:
+          "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+        primaryOutline: "bg-white text-sky-500  hover:bg-slate-100",
+        secondary:
+          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0",
+        secondaryOutline: "bg-white text-green-500  hover:bg-slate-100",
+        danger:
+          "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
+        dangerOutline: "bg-white text-rose-500  hover:bg-slate-100",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
+        superOutline: "bg-white text-indigo-500  hover:bg-slate-100",
       },
       size: {
         default: "h-11 px-4 py-2",
