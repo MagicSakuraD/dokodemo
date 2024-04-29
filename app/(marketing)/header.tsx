02827bfad2a5e2ea08cdfb99744873fdc804a136
MagicSakuraD <ckgoforit@outlook.com>
2024-04-29 09:20:45 +0800

    Update header and page components with new dependencies and UI improvements

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div
+        className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full
+      "
+      >
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/Cat Face.png" alt="Cat Face" width={50} height={50} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Quinn
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignIn,
+  SignedOut,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row  justify-center items-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/react.svg" fill alt="Hero" />
+      </div>
+      <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+        Learn, practice, and master music
+      </h1>
+      <div>
+        <ClerkLoading>
+          <Loader className="w-5 h-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignUpButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant={"secondary"} className="w-full">
+                开始
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+          <SignIn></SignIn>
+        </ClerkLoaded>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
