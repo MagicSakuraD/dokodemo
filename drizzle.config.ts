6272f2be2cfe912ad733393b37e5d6bc4eb29d62
MagicSakuraD <ckgoforit@outlook.com>
2024-05-17 11:04:36 +0800

    Update database URL in drizzle.ts and drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,11 @@
 import "dotenv/config";
-import type { Config } from "drizzle-kit";
 import { defineConfig } from "drizzle-kit";
-
-// export default {
-//   schema: "./db/schema.ts",
-//   out: "./drizzle",
-//   driver: "pg",
-//   dbCredentials: {
-//     connectionString: process.env.DATABASE_URL!,
-//   },
-// } satisfies Config;
 
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql", // 'postgresql' | 'mysql' | 'sqlite'
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.NEON_DATABASE_URL!,
   },
 });
